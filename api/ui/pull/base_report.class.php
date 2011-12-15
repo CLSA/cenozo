53a5426314931476f6b1eef16e5749af03016d4f
Patrick Emond <emondpd@mcmaster.ca>
2011-12-15 15:01:17 -0500

    Adding timezone to report creation time

--- api/ui/pull/base_report.class.php
+++ api/ui/pull/base_report.class.php
@@ -117,7 +117,7 @@ abstract class base_report extends \cenozo\ui\pull
 
     $now_datetime_obj = $util_class_name::get_datetime_object();
     $time_title = 'Generated on '.$now_datetime_obj->format( 'Y-m-d' ).
-                   ' at '.$now_datetime_obj->format( 'H:i' );
+                   ' at '.$now_datetime_obj->format( 'H:i T' );
     $this->report->set_size( 14 );
     $this->report->set_bold( false );
     if( $max_col ) $this->report->merge_cells( 'A'.$row.':'.$max_col.$row );
