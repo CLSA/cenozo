51ffd9c5f646a0e6efa9e104493aa8849afbe722
Patrick Emond <emondpd@mcmaster.ca>
2012-02-10 10:29:51 -0500

    Fixing non-critical bug in base_report pull operation

--- api/ui/pull/base_report.class.php
+++ api/ui/pull/base_report.class.php
@@ -277,7 +277,7 @@ abstract class base_report extends \cenozo\ui\pull
       $first_content_row = $row;
 
       // put in the table contents
-      unset( $contents_are_numeric );
+      $contents_are_numeric = array();
       if( count( $table['contents'] ) )
       {
         $content_row = 0;
