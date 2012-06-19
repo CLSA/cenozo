0cf204127fb544e3f403eace155549167a0fa51a
Patrick Emond <emondpd@mcmaster.ca>
2012-06-19 18:07:40 -0400

    Adding modifier to pull arguments

--- api/ui/pull/base_list.class.php
+++ api/ui/pull/base_list.class.php
@@ -31,63 +31,6 @@ abstract class base_list extends \cenozo\ui\pull
   }
 
   /**
-   * Processes arguments, preparing them for the operation.
-   * 
-   * @author Dean Inglis <inglisd@mcmaster.ca>
-   * @access protected
-   */
-  protected function prepare()
-  {
-    parent::prepare();
-
-    $this->restrictions = array();
-    $restrictions = $this->get_argument( 'restrictions', array() );
-    
-    if( is_array( $restrictions ) ) foreach( $restrictions as $column => $restrict )
-    {
-      $operator = NULL;
-      $value = NULL;
-
-      if( array_key_exists( 'value', $restrict ) && array_key_exists( 'compare', $restrict ) )
-      {
-        $value = $restrict['value'];
-        if( 'is' == $restrict['compare'] )
-        {
-          $operator = '=';
-        }
-        else if( 'is not' == $restrict['compare'] )
-        {
-          $operator = '!=';
-        }
-        else if( 'like' == $restrict['compare'] )
-        {
-          $value = '%'.$value.'%';
-          $operator = 'LIKE';
-        }
-        else if( 'not like' == $restrict['compare'] )
-        {
-          $value = '%'.$value.'%';
-          $operator = 'NOT LIKE';
-        }
-        else if( 'in' == $restrict['compare'] )
-        {
-          if( !is_array( $value ) ) $value = explode( ',', $value );
-          $operator = 'IN';
-        }
-        else log::err( 'Invalid comparison in list restriction.' );
-      }
-
-      if( !is_null( $operator ) && !is_null( $value ) )
-      {
-        $this->restrictions[] = array(
-          'column' => $column,
-          'operator' => $operator,
-          'value' => 'NULL' == $value ? NULL : $value );
-      }
-    }
-  }
-  
-  /**
    * This method executes the operation's purpose.
    * 
    * @author Dean Inglis <inglisd@mcmaster.ca>
@@ -97,19 +40,18 @@ abstract class base_list extends \cenozo\ui\pull
   {
     parent::execute();
 
-    $modifier = lib::create( 'database\modifier' );
-    foreach( $this->restrictions as $restrict )
-      $modifier->where( $restrict['column'], $restrict['operator'], $restrict['value'] );
-
-    $limit = $this->get_argument( 'limit', NULL );
-    $offset = $this->get_argument( 'offset', 0 );
-    if( !is_null( $limit ) ) $modifier->limit( $limit, $offset );
-
     $class_name = lib::get_class_name( 'database\\'.$this->get_subject() );
-    $this->data = array();
 
-    foreach( $class_name::select( $modifier ) as $record )
-      $this->data[] = $this->process_record( $record );
+    if( $this->get_argument( 'count', false ) )
+    { // get a count only
+      $this->data = $class_name::count( $this->modifier );
+    }
+    else
+    { // build an array of all records
+      $this->data = array();
+      foreach( $class_name::select( $this->modifier ) as $record )
+        $this->data[] = $this->process_record( $record );
+    }
   }
 
   /**
@@ -135,12 +77,5 @@ abstract class base_list extends \cenozo\ui\pull
    * @access public
    */
   public function get_data_type() { return "json"; }
-
-  /**
-   * An associative array of restrictions to apply to the list.
-   * @var array
-   * @access protected
-   */
-  protected $restrictions = array();
 }
 ?>
