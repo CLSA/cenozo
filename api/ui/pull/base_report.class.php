d52f98070b4537eec57aea784fed1b91ad66f2d7
Patrick Emond <emondpd@mcmaster.ca>
2012-09-20 14:21:31 -0400

    Fixing bug in base_report: report template path

--- api/ui/pull/base_report.class.php
+++ api/ui/pull/base_report.class.php
@@ -44,7 +44,7 @@ abstract class base_report extends \cenozo\ui\pull
     parent::prepare();
 
     // check to see if a template exists for this report
-    $filename = sprintf( '%s/report/template/%s.xls', DOC_PATH, $this->get_full_name() );
+    $filename = sprintf( '%s/report/%s.xls', DOC_PATH, $this->get_full_name() );
     $this->report = lib::create( 'business\report', file_exists( $filename ) ? $filename : NULL );
   }
 
