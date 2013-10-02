7e6209d7eae81dfb87acf2aaaa17aa1c5cc938e4
Patrick Emond <emondpd@mcmaster.ca>
2013-10-02 17:14:37 -0400

    More fixes to participant report

--- api/ui/pull/participant_report.class.php
+++ api/ui/pull/participant_report.class.php
@@ -110,55 +110,118 @@ class participant_report extends \cenozo\ui\pull\base_report
       unset( $temp_obj );
     }
 
-    // define the modifier based on the report parameters
+    // define the sql tables, columns and modifier based on the report parameters
+    $this->sql_tables =
+      'FROM participant '.
+      'JOIN source ON participant.source_id = source.id '.
+      'JOIN cohort ON participant.cohort_id = cohort.id '.
+      'LEFT JOIN participant_primary_address '.
+      'ON participant.id = participant_primary_address.participant_id '.
+      'LEFT JOIN address ON participant_primary_address.address_id = address.id '.
+      'LEFT JOIN region ON address.region_id = region.id ';
+
+    $this->sql_columns =
+      'SELECT participant.uid, '.
+      ( '' === $active ? 'IF( participant.active, "Yes", "No" ) active, ' : '' ).
+      ( '' === $source_id ? 'source.name AS source_name, ' : '' ).
+      ( '' === $cohort_id ? 'cohort.name AS cohort_name, ' : '' ).
+      'participant.first_name, '.
+      'participant.last_name, ';
+
     $this->modifier->order( 'uid' );
 
     if( '' !== $source_id ) $this->modifier->where( 'participant.source_id', '=', $source_id );
     if( '' !== $cohort_id ) $this->modifier->where( 'participant.cohort_id', '=', $cohort_id );
     if( '' !== $active ) $this->modifier->where( 'participant.active', '=', $active );
-    foreach( $site_id_list as $service_id => $site_id )
+
+    foreach( $service_class_name::select() as $db_service )
     {
-      if( '' !== $site_id )
+      if( $db_service->get_site_count() )
       {
-        $service_name = lib::create( 'database\service', $service_id )->name;
-        if( -1 == $site_id )
+        $this->sql_tables .= sprintf(
+          'LEFT JOIN participant_site AS %s_ps '.
+          'ON participant.id = %s_ps.participant_id '.
+          'AND %s_ps.service_id = %d '.
+          'LEFT JOIN site AS %s_site ON %s_ps.site_id = %s_site.id ',
+          $db_service->name,
+          $db_service->name,
+          $db_service->name,
+          $db_service->id,
+          $db_service->name,
+          $db_service->name,
+          $db_service->name );
+
+        $this->sql_columns .= sprintf(
+          '%s_site.name AS %s_site_name, ',
+          $db_service->name,
+          $db_service->name );
+
+        // restrict, if necessary
+        $site_id = $site_id_list[$db_service->id];
+        if( '' !== $site_id )
         {
-          $this->modifier->where( $service_name.'_participant_site.service_id', '=', $service_id );
-          $this->modifier->where( $service_name.'_participant_site.site_id', '=', NULL );
-        }
-        else
-        {
-          $this->modifier->where( $service_name.'_participant_site.service_id', '=', $service_id );
-          $this->modifier->where( $service_name.'_participant_site.site_id', '=', $site_id );
+          $service_name = $db_service->name;
+          if( -1 == $site_id )
+          {
+            $this->modifier->where( $service_name.'_ps.service_id', '=', $db_service->id );
+            $this->modifier->where( $service_name.'_ps.site_id', '=', NULL );
+          }
+          else
+          {
+            $this->modifier->where( $service_name.'_ps.service_id', '=', $db_service->id );
+            $this->modifier->where( $service_name.'_ps.site_id', '=', $site_id );
+          }
         }
       }
     }
+
     foreach( $released_list as $service_id => $released )
     {
       if( '' !== $released )
       {
+        $service_name = lib::create( 'database\service', $service_id )->name;
         if( $released )
         {
-          $this->modifier->where( 'service_has_participant.datetime', '!=', NULL );
+          $this->modifier->where( $service_name.'_shp.datetime', '!=', NULL );
+          $this->sql_tables .= sprintf(
+            'JOIN service_has_participant AS %s_shp '.
+            'ON participant.id = %s_shp.participant_id '.
+            'AND %s_shp.service_id = %d ',
+            $service_name,
+            $service_name,
+            $service_name,
+            $service_id );
         }
         else // not released means the participant may not be in the service_has_participant table
         {
           $sql = sprintf(
             'SELECT participant.id FROM participant '.
             'JOIN service_has_participant '.
-            'ON service_has_participant.participant_id = participant.id '.
-            'AND service_has_participant.service_id = %s '.
+            'ON %s_shp.participant_id = participant.id '.
+            'AND %s_shp.service_id = %s '.
             'WHERE datetime IS NOT NULL',
+            $service_name,
+            $service_name,
             $database_class_name::format_string( $session->get_site()->get_service()->id ) );
 
           $this->modifier->where( 'participant.id', 'NOT IN', sprintf( '( %s )', $sql ), false );
         }
       }
     }
+
     if( '' !== $region_id ) $this->modifier->where( 'address.region_id', '=', $region );
+
     if( '' !== $gender ) $this->modifier->where( 'participant.gender', '=', $gender );
+    else $this->sql_columns .= 'participant.gender, ';
+
     if( '' !== $age_group_id )
       $this->modifier->where( 'participant.age_group_id', '=', $age_group_id );
+    else
+    {
+      $this->sql_tables .= 'LEFT JOIN age_group ON participant.age_group_id = age_group.id ';
+      $this->sql_columns .= 'CONCAT( age_group.lower, " to ", age_group.upper ) AS age_group, ';
+    }
+
     if( '' !== $date_of_birth_start_date )
       $this->modifier->where( 'participant.date_of_birth', '>=',
                                $date_of_birth_start_obj->format( 'Y-m-d' ) );
@@ -171,14 +234,25 @@ class participant_report extends \cenozo\ui\pull\base_report
       else if( 'none' == $status ) $this->modifier->where( 'participant.status', '=', NULL );
       else $this->modifier->where( 'participant.status', '=', $status );
     }
-    if( '' !== $language ) $this->modifier->where( 'participant.language', '=', $language );
+    else $this->sql_columns .= 'participant.status AS `condition`, ';
+
+    if( '' !== $language )
+      $this->modifier->where( 'IFNULL( participant.language, "en" )', '=', $language );
+    else $this->sql_columns .= 'IFNULL( participant.language, "en" ) AS language, ';
+
     if( '' !== $consent_accept || '' !== $consent_written )
-      $this->modifier->where( 'participant_last_consent.consent_id', '=', 'consent.id', false );
+    {
+      $this->sql_tables .=
+        'JOIN participant_last_consent '.
+        'ON participant.id = participant_last_consent.participant_id ';
+    }
     if( '' !== $consent_accept )
-      $this->modifier->where( 'consent.accept', '=', $consent_accept );
+      $this->modifier->where( 'participant_last_consent.accept', '=', $consent_accept );
     if( '' !== $consent_written )
-      $this->modifier->where( 'consent.written', '=', $consent_written );
+      $this->modifier->where( 'participant_last_consent.written', '=', $consent_written );
     
+    if( '' !== $event_type_id || '' !== $event_start_date || '' !== $event_end_date )
+      $this->sql_tables .= 'LEFT JOIN event ON participant.id = event.participant_id ';
     if( '' !== $event_type_id )
       $this->modifier->where( 'event.event_type_id', '=', $event_type_id );
     if( '' !== $event_start_date )
@@ -204,68 +278,9 @@ class participant_report extends \cenozo\ui\pull\base_report
       $this->modifier->where( $address_count, '=', sprintf( '( %s )', $sql ), false );
     }
 
-    $this->sql_tables =
-      'FROM participant '.
-      'JOIN source ON participant.source_id = source.id '.
-      'JOIN cohort ON participant.cohort_id = cohort.id ';
-
-    $this->sql_columns =
-      'SELECT participant.uid, '.
-      ( '' === $active ? 'IF( participant.active, "Yes", "No" ) active, ' : '' ).
-      ( '' === $source_id ? 'source.name AS source_name, ' : '' ).
-      ( '' === $cohort_id ? 'cohort.name AS cohort_name, ' : '' ).
-      'participant.first_name, '.
-      'participant.last_name, ';
-
-    foreach( $service_class_name::select() as $db_service )
-    {
-      if( $db_service->get_site_count() )
-      {
-        $this->sql_tables .= sprintf(
-          'LEFT JOIN participant_site AS %s_participant_site '.
-          'ON participant.id = %s_participant_site.participant_id '.
-          'AND %s_participant_site.service_id = %d '.
-          'LEFT JOIN site AS %s_site ON %s_participant_site.site_id = %s_site.id ',
-          $db_service->name,
-          $db_service->name,
-          $db_service->name,
-          $db_service->id,
-          $db_service->name,
-          $db_service->name,
-          $db_service->name );
-
-        $this->sql_columns .= sprintf(
-          '%s_site.name AS %s_site_name, ',
-          $db_service->name,
-          $db_service->name );
-      }
-    }
-
-    if( '' !== $event_type_id || '' !== $event_start_date || '' !== $event_end_date )
-    {
-      $this->sql_tables .= 'LEFT JOIN event ON participant.id = event.participant_id ';
-    }
-
-    // now build the column and table parts of the custom sql
-    $this->sql_columns .= '' === $gender ? 'participant.gender, ' : '';
-
-    if( '' === $age_group_id )
-    {
-      $this->sql_tables .= 'LEFT JOIN age_group ON participant.age_group_id = age_group.id ';
-      $this->sql_columns .= 'CONCAT( age_group.lower, " to ", age_group.upper ) AS age_group, ';
-    }
-
-    $this->sql_columns .= '' === $status ? 'participant.status AS `condition`, ' : '';
-    $this->sql_columns .= '' === $language ? 'IFNULL( participant.language, "en" ) AS language, ' : '';
-
+    // if we're not looking at address count then add the address columns
     if( '' === $address_count || 0 != $address_count )
     {
-      $this->sql_tables .=
-        'LEFT JOIN participant_primary_address '.
-        'ON participant.id = participant_primary_address.participant_id '.
-        'LEFT JOIN address ON participant_primary_address.address_id = address.id '.
-        'LEFT JOIN region ON address.region_id = region.id ';
-
       $this->sql_columns .=
         'address.address1, '.
         'address.address2, '.
@@ -275,12 +290,12 @@ class participant_report extends \cenozo\ui\pull\base_report
         'address.postcode, ';
     }
 
+    // if we're not looking at phone count then add the phone column
     if( '' === $phone_count || 0 != $phone_count )
     {
       $this->sql_tables .=
         'LEFT JOIN phone ON participant.person_id = phone.person_id '.
         'AND phone.rank = 1 ';
-
       $this->sql_columns .= 'phone.number AS phone_number, ';
     }
     
