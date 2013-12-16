6e793f1dd96512ceeb8ff0ce947e0e02b35fb69f
Patrick Emond <emondpd@mcmaster.ca>
2013-12-16 10:25:54 -0500

    Preventing changes to positive consent after withdraw
    Once participant.withdraw_letter is set no changes can be made to a
    participant's positive consent status.  This is to prevent them from
    being automatically re-added after they have withdrawn.

--- api/database/consent.class.php
+++ api/database/consent.class.php
@@ -15,6 +15,26 @@ use cenozo\lib, cenozo\log;
 class consent extends record
 {
   /**
+   * Override parent save method by not allowing new consent records once a participant's
+   * withdraw letter has been set
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @throws exception\permission
+   * @access public
+   */
+  public function save()
+  {
+    if( $this->accept && !is_null( $this->get_participant()->withdraw_letter ) )
+    {
+      throw lib::create( 'exception\notice',
+        'The participant has completed the withdraw script, '.
+        'no changes to consent status are allowed.', __METHOD__ );
+    }
+
+    parent::save();
+  }
+  
+  /**
    * Returns a string representation of the consent (eg: verbal deny, written accept, etc)
    * @author Patrick Emond <emondpd@mcmaster.ca>
    * @return string
