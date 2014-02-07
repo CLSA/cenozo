f68e91156b88bed112c2f142747bb8af8cc33072
Patrick Emond <emondpd@mcmaster.ca>
2014-02-07 13:46:22 -0500

    Bumping development to version 0.2.5

--- app/settings.ini.php
+++ app/settings.ini.php
@@ -13,7 +13,7 @@
 $settings = array();
 
 // Framework software version (is never overridded by the application's ini file)
-$settings['general']['cenozo_version'] = '0.2.4';
+$settings['general']['cenozo_version'] = '0.2.5';
 
 // When set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
