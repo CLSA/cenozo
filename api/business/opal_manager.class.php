67867877c73dc7c09d8b42fff9676dc2644ae1dc
Patrick Emond <emondpd@mcmaster.ca>
2014-03-20 17:03:44 -0400

    Adding get_label() method to opal_manager
    Can be used to return the label value of a particular variable's value.

--- api/business/opal_manager.class.php
+++ api/business/opal_manager.class.php
@@ -44,8 +44,6 @@ class opal_manager extends \cenozo\factory
    */
   public function get_value( $datasource, $table, $db_participant, $variable )
   {
-    $util_class_name = lib::get_class_name( 'util' );
-
     if( is_null( $db_participant ) )
       throw lib::create( 'exception\argument', 'db_participant', $db_participant, __METHOD__ );
     else if( 0 == strlen( $variable ) )
@@ -96,6 +94,89 @@ class opal_manager extends \cenozo\factory
     }
 
     return $message->body;
+  }
+
+  /**
+   * Get a label for a particular variable's value
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param string $datasource The datasource to get a value from
+   * @param string $table The table to get a value from
+   * @param database\participant $db_participant The participant to get a value from
+   * @param string $variable The name of the variable to get the label for
+   * @param string $value The value of the variable to get the label for
+   * @param string $locale Which locale (language) to return the label in
+   * @return string
+   * @throws exception\argument, exception\runtime
+   * @access public
+   */
+  public function get_label( $datasource, $table, $variable, $value, $locale )
+  {
+    $util_class_name = lib::get_class_name( 'util' );
+
+    if( 0 == strlen( $variable ) )
+      throw lib::create( 'exception\argument', 'variable', $variable, __METHOD__ );
+    else if( 0 == strlen( $value ) )
+      throw lib::create( 'exception\argument', 'value', $value, __METHOD__ );
+    else if( 0 == strlen( $locale ) )
+      throw lib::create( 'exception\argument', 'locale', $locale, __METHOD__ );
+
+    // prepare the http request
+    $authorization = sprintf( '%s:%s', $this->username, $this->password );
+    $request = new \HttpRequest();
+    $request->setMethod( \HttpRequest::METH_GET );
+    $request->addHeaders( array(
+      'Authorization' => 'X-Opal-Auth '.base64_encode( $authorization ),
+      'Accept' => 'application/json' ) );
+
+    // send the http request
+    $url = sprintf(
+      'https://%s:%d/ws/datasource/%s/table/%s/variable/%s',
+      $this->server,
+      $this->port,
+      rawurlencode( $datasource ),
+      rawurlencode( $table ),
+      rawurlencode( $variable ) );
+    $request->setUrl( $url );
+    
+    try
+    {
+      $message = $request->send();
+    }
+    catch( \Exception $e )
+    {
+      // We've caught one of HttpRuntime, HttpRequest, HttpMalformedHeader or HttpEncoding Exceptions
+      // These errors may be transient, so instruct the user to reload the page
+      throw lib::create( 'exception\notice',
+        'The server appears to be busy, please try reloading the page. '.
+        'If this still does not fix the problem please report the issue to a superior.',
+        __METHOD__ );
+    }
+
+    $code = $message->getResponseCode();
+    if( 200 != $code )
+    {
+      throw lib::create( 'exception\runtime',
+        sprintf( 'Unable to connect to Opal service for url "%s" (code: %s)', $url, $code ),
+        __METHOD__ );
+    }
+
+    // find the variable in the response
+    $object = $util_class_name::json_decode( $message->body );
+    if( !is_object( $object ) || !property_exists( $object, 'categories' ) )
+      throw lib::create( 'exception\runtime',
+        sprintf( 'Unrecognized response from Opal service for url "%s"', $url ),
+        __METHOD__ );
+
+    $label = NULL;
+    foreach( $object->categories as $category )
+      if( $value == $category->name )
+        foreach( $category->attributes as $attribute )
+          if( 'label' == $attribute->name && $locale == $attribute->locale )
+            return utf8_decode( $attribute->value );
+
+    log::warning( sprintf( 'Label of Opal variable "%s" was not found.', $variable ) );
+    return NULL;
   }
 
   /**
