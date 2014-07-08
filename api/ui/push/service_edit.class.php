e0cf138068137e9dbdf46041163799bc122df088
Patrick Emond <emondpd@mcmaster.ca>
2014-07-08 15:44:27 -0400

    Adding better checks for service name when adding/editing

--- api/ui/push/service_edit.class.php
+++ api/ui/push/service_edit.class.php
@@ -28,6 +28,32 @@ class service_edit extends base_edit
   }
 
   /**
+   * Validate the operation.
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @throws exception\notice
+   * @access protected
+   */
+  protected function validate()
+  {
+    parent::validate();
+
+    // make sure the name column isn't blank
+    $columns = $this->get_argument( 'columns' );
+    if( array_key_exists( 'name', $columns ) )
+    {
+      if( 0 == strlen( $columns['name'] ) )
+        throw lib::create( 'exception\notice',
+          'The service\'s name cannot be left blank.', __METHOD__ );
+
+      // make sure the name column contains letters, numbers and underscores only
+      if( preg_match( '/[^a-zA-Z0-9_]/', $columns['name'] ) )
+        throw lib::create( 'exception\notice',
+          'The service\'s name can include letters, numbers and underscores only.', __METHOD__ );
+    }
+  }
+
+  /**
    * Finishes the operation with any post-execution instructions that may be necessary.
    * 
    * @author Patrick Emond <emondpd@mcmaster.ca>
