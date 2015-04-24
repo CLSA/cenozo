f859ed963153adbd677cb2de9aaa100b740dbd38
Patrick Emond <emondpd@mcmaster.ca>
2015-04-24 12:11:50 -0400

    Modifying jurisdiction and region_site tables
    The application_id columns have been removed and triggers have been
    added to emulate constraints on the site-based unique keys such that
    entries are restricted by application, not only site.

--- db/upgrade/2.0.0/jurisdiction.sql
+++ db/upgrade/2.0.0/jurisdiction.sql
@@ -3,36 +3,23 @@ DELIMITER //
 CREATE PROCEDURE patch_jurisdiction()
   BEGIN
 
-    SELECT "Renaming service_id column to application_id in jurisdiction table" AS "";
+    SELECT "Removing service_id column from jurisdiction table" AS "";
 
     SET @test = (
       SELECT COUNT(*)
       FROM information_schema.COLUMNS
       WHERE TABLE_SCHEMA = DATABASE()
       AND TABLE_NAME = "jurisdiction"
-      AND COLUMN_NAME = "application_id" );
-    IF @test = 0 THEN
-      -- drop foreign keys
+      AND COLUMN_NAME = "service_id" );
+    IF @test = 1 THEN
+      -- drop foreign keys, key and column
       ALTER TABLE jurisdiction
-      DROP FOREIGN KEY fk_jurisdiction_service_id;
-
-      -- rename column
-      ALTER TABLE jurisdiction
-      CHANGE service_id application_id INT UNSIGNED NOT NULL;
-
-      -- rename keys
-      ALTER TABLE jurisdiction
+      DROP FOREIGN KEY fk_jurisdiction_service_id,
       DROP KEY fk_service_id,
-      ADD KEY fk_application_id (application_id);
-
-      ALTER TABLE jurisdiction
+      DROP COLUMN service_id,
       DROP KEY uq_service_id_postcode,
-      ADD UNIQUE KEY uq_application_id_postcode (application_id ASC, postcode ASC);
-
-      ALTER TABLE jurisdiction
-      ADD CONSTRAINT fk_jurisdiction_application_id
-      FOREIGN KEY (application_id) REFERENCES application (id)
-      ON DELETE NO ACTION ON UPDATE NO ACTION;
+      ADD UNIQUE KEY uq_site_id_postcode (site_id ASC, postcode ASC),
+      MODIFY COLUMN site_id INT(10) UNSIGNED NOT NULL AFTER create_timestamp;
     END IF;
 
   END //
@@ -40,3 +27,58 @@ DELIMITER ;
 
 CALL patch_jurisdiction();
 DROP PROCEDURE IF EXISTS patch_jurisdiction;
+
+SELECT "Adding new triggers to jurisdiction table" AS "";
+
+-- create trigger to enforce application-postcode pseudo unique key
+DELIMITER //
+DROP TRIGGER IF EXISTS jurisdiction_BEFORE_INSERT //
+CREATE TRIGGER jurisdiction_BEFORE_INSERT
+BEFORE INSERT ON jurisdiction FOR EACH ROW
+BEGIN
+  SET @test = (
+    SELECT COUNT(*) FROM jurisdiction
+    JOIN site ON jurisdiction.site_id = site.id
+    WHERE jurisdiction.postcode = NEW.postcode
+    AND site.application_id = (
+      SELECT application_id FROM site
+      WHERE id = NEW.site_id
+    )
+  );
+  IF @test > 0 THEN
+    -- trigger unique key conflict
+    SET @sql = CONCAT(
+      "Duplicate entry '",
+      NEW.site_id, "-", NEW.postcode,
+      "' for key 'uq_site_id_postcode'"
+    );
+    SIGNAL SQLSTATE '23000' SET MESSAGE_TEXT = @sql, MYSQL_ERRNO = 1062;
+  END IF;
+END;//
+
+DROP TRIGGER IF EXISTS jurisdiction_BEFORE_UPDATE //
+CREATE TRIGGER jurisdiction_BEFORE_UPDATE
+BEFORE UPDATE ON jurisdiction FOR EACH ROW
+BEGIN
+  SET @test = (
+    SELECT COUNT(*) FROM jurisdiction
+    JOIN site ON jurisdiction.site_id = site.id
+    WHERE jurisdiction.postcode = NEW.postcode
+    AND site.application_id = (
+      SELECT application_id FROM site
+      WHERE id = NEW.site_id
+    )
+    AND jurisdiction.id != NEW.id
+  );
+  IF @test > 0 THEN
+    -- trigger unique key conflict
+    SET @sql = CONCAT(
+      "Duplicate entry '",
+      NEW.site_id, "-", NEW.postcode,
+      "' for key 'uq_site_id_postcode'"
+    );
+    SIGNAL SQLSTATE '23000' SET MESSAGE_TEXT = @sql, MYSQL_ERRNO = 1062;
+  END IF;
+END;//
+
+DELIMITER ;
--- db/upgrade/2.0.0/region_site.sql
+++ db/upgrade/2.0.0/region_site.sql
@@ -3,36 +3,23 @@ DELIMITER //
 CREATE PROCEDURE patch_region_site()
   BEGIN
 
-    SELECT "Renaming service_id column to application_id in region_site table" AS "";
+    SELECT "Removing service_id column from region_site table" AS "";
 
     SET @test = (
       SELECT COUNT(*)
       FROM information_schema.COLUMNS
       WHERE TABLE_SCHEMA = DATABASE()
       AND TABLE_NAME = "region_site"
-      AND COLUMN_NAME = "application_id" );
-    IF @test = 0 THEN
-      -- drop foreign keys
+      AND COLUMN_NAME = "service_id" );
+    IF @test = 1 THEN
+      -- drop foreign keys, key and column
       ALTER TABLE region_site
-      DROP FOREIGN KEY fk_region_site_service_id;
-
-      -- rename column
-      ALTER TABLE region_site
-      CHANGE service_id application_id INT UNSIGNED NOT NULL;
-
-      -- rename keys
-      ALTER TABLE region_site
+      DROP FOREIGN KEY fk_region_site_service_id,
       DROP KEY fk_service_id,
-      ADD KEY fk_application_id (application_id);
-
-      ALTER TABLE region_site
+      DROP COLUMN service_id,
       DROP KEY uq_service_id_region_id_language_id,
-      ADD UNIQUE KEY `uq_application_id_region_id_language_id` (`application_id` ASC, `region_id` ASC, `language_id` ASC);
-
-      ALTER TABLE region_site
-      ADD CONSTRAINT fk_region_site_application_id
-      FOREIGN KEY (application_id) REFERENCES application (id)
-      ON DELETE NO ACTION ON UPDATE NO ACTION;
+      ADD UNIQUE KEY uq_site_id_region_id_language_id (site_id ASC, region_id ASC, language_id ASC),
+      MODIFY COLUMN site_id INT(10) UNSIGNED NOT NULL AFTER create_timestamp;
     END IF;
 
   END //
@@ -40,3 +27,60 @@ DELIMITER ;
 
 CALL patch_region_site();
 DROP PROCEDURE IF EXISTS patch_region_site;
+
+SELECT "Adding new triggers to region_site table" AS "";
+
+-- create trigger to enforce application-region-language pseudo unique key
+DELIMITER //
+DROP TRIGGER IF EXISTS region_site_BEFORE_INSERT //
+CREATE TRIGGER region_site_BEFORE_INSERT
+BEFORE INSERT ON region_site FOR EACH ROW
+BEGIN
+  SET @test = (
+    SELECT COUNT(*) FROM region_site
+    JOIN site ON region_site.site_id = site.id
+    WHERE region_site.region_id = NEW.region_id
+    AND region_site.language_id = NEW.language_id
+    AND site.application_id = (
+      SELECT application_id FROM site
+      WHERE id = NEW.site_id
+    )
+  );
+  IF @test > 0 THEN
+    -- trigger unique key conflict
+    SET @sql = CONCAT(
+      "Duplicate entry '",
+      NEW.site_id, "-", NEW.region_id, "-", NEW.language_id,
+      "' for key 'uq_site_id_region_id_language_id'"
+    );
+    SIGNAL SQLSTATE '23000' SET MESSAGE_TEXT = @sql, MYSQL_ERRNO = 1062;
+  END IF;
+END;//
+
+DROP TRIGGER IF EXISTS region_site_BEFORE_UPDATE //
+CREATE TRIGGER region_site_BEFORE_UPDATE
+BEFORE UPDATE ON region_site FOR EACH ROW
+BEGIN
+  SET @test = (
+    SELECT COUNT(*) FROM region_site
+    JOIN site ON region_site.site_id = site.id
+    WHERE region_site.region_id = NEW.region_id
+    AND region_site.language_id = NEW.language_id
+    AND site.application_id = (
+      SELECT application_id FROM site
+      WHERE id = NEW.site_id
+    )
+    AND region_site.id != NEW.id
+  );
+  IF @test > 0 THEN
+    -- trigger unique key conflict
+    SET @sql = CONCAT(
+      "Duplicate entry '",
+      NEW.site_id, "-", NEW.region_id, "-", NEW.language_id,
+      "' for key 'uq_site_id_region_id_language_id'"
+    );
+    SIGNAL SQLSTATE '23000' SET MESSAGE_TEXT = @sql, MYSQL_ERRNO = 1062;
+  END IF;
+END;//
+
+DELIMITER ;
