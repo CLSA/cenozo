d42e82ef000865a20c6c7652db1b16e73c205e09
Patrick Emond <emondpd@mcmaster.ca>
2015-05-01 15:36:44 -0400

    Adding 'types' as field to the alternate query service

--- api/service/alternate/module.class.php
+++ api/service/alternate/module.class.php
@@ -29,4 +29,24 @@ class module extends \cenozo\service\module
 
     return $valid;
   }
+
+  /**
+   * Extend parent method
+   */
+  public function prepare_read( $select, $modifier )
+  {
+    $util_class_name = lib::get_class_name( 'util' );
+
+    parent::prepare_read( $select, $modifier );
+
+    // add the "types" column if needed
+    if( $select->has_column( 'types' ) )
+    {
+      $column = sprintf( 'REPLACE( TRIM( CONCAT( %s, %s, %s ) ), "  ", ", " )',
+                  'IF( alternate, " alternate ", "" )',
+                  'IF( informant, " informant ", "" )',
+                  'IF( proxy, " proxy ", "" )' );
+      $select->add_column( $column, 'types', false );
+    }
+  }
 }
