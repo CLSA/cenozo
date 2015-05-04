916ab7cd2892488ec182b7ce4c4d8f6564129ff6
Patrick Emond <emondpd@mcmaster.ca>
2015-05-04 14:42:38 -0400

    Restricting access to application

--- api/service/access/module.class.php
+++ api/service/access/module.class.php
@@ -29,4 +29,16 @@ class module extends \cenozo\service\module
 
     return $valid;
   }
+
+  /**
+   * Extend parent method
+   */
+  public function prepare_read( $select, $modifier )
+  {
+    parent::prepare_read( $select, $modifier );
+
+    // only include sites which belong to this application
+    $modifier->join( 'site', 'access.site_id', 'site.id' );
+    $modifier->where( 'site.application_id', '=', lib::create( 'business\session' )->get_application()->id );
+  }
 }
