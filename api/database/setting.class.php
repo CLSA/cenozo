3a0ddc45818ecf10aee0ced5aad5c4d4a93be9ec
Patrick Emond <emondpd@mcmaster.ca>
2015-07-08 19:04:49 -0400

    Fixing bugs in last commit

---
+++ api/database/setting.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * setting.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ * @filesource
+ */
+
+namespace cenozo\database;
+use cenozo\lib, cenozo\log;
+
+/**
+ * setting: record
+ */
+class setting extends record {}
