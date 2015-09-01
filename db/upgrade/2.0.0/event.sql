af6802e29e0bd06dc6fa46a973946e71fa7b3601
Patrick Emond <emondpd@mcmaster.ca>
2015-09-01 15:19:29 -0400

    Changing ON DELETE rule to CASCADE for participant and event_type in event table.

--- db/upgrade/2.0.0/event.sql
+++ db/upgrade/2.0.0/event.sql
@@ -1,3 +1,53 @@
+DROP PROCEDURE IF EXISTS patch_event;
+  DELIMITER //
+  CREATE PROCEDURE patch_event()
+  BEGIN
+
+    SELECT "Modifiying constraint delete rules in event table" AS "";
+
+    SET @test = (
+      SELECT DELETE_RULE
+      FROM information_schema.REFERENTIAL_CONSTRAINTS
+      WHERE CONSTRAINT_SCHEMA = DATABASE()
+      AND TABLE_NAME = "event"
+      AND REFERENCED_TABLE_NAME = "participant" );
+    IF @test = "NO ACTION" THEN
+      ALTER TABLE event
+      DROP FOREIGN KEY fk_event_participant_id;
+
+      ALTER TABLE event
+      ADD CONSTRAINT fk_event_participant_id
+      FOREIGN KEY (participant_id)
+      REFERENCES participant (id)
+      ON DELETE CASCADE
+      ON UPDATE NO ACTION;
+    END IF;
+
+    SET @test = (
+      SELECT DELETE_RULE
+      FROM information_schema.REFERENTIAL_CONSTRAINTS
+      WHERE CONSTRAINT_SCHEMA = DATABASE()
+      AND TABLE_NAME = "event"
+      AND REFERENCED_TABLE_NAME = "event_type" );
+    IF @test = "NO ACTION" THEN
+      ALTER TABLE event
+      DROP FOREIGN KEY fk_event_event_type_id;
+
+      ALTER TABLE event
+      ADD CONSTRAINT fk_event_event_type_id
+      FOREIGN KEY (event_type_id)
+      REFERENCES event_type (id)
+      ON DELETE CASCADE
+      ON UPDATE NO ACTION;
+    END IF;
+
+  END //
+DELIMITER ;
+
+-- now call the procedure and remove the procedure
+CALL patch_event();
+DROP PROCEDURE IF EXISTS patch_event;
+
 SELECT "Adding new triggers to event table" AS "";
 
 DELIMITER $$
