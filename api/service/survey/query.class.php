3dd120ddc92144c2f34eb8429b0bb9a17ac95fbf
Patrick Emond <emondpd@mcmaster.ca>
2015-10-06 17:12:19 -0400

    Fixing bug in service\survey\query class

--- api/service/survey/query.class.php
+++ api/service/survey/query.class.php
@@ -27,10 +27,24 @@ class query extends \cenozo\service\query
   /**
    * Extends parent method
    */
-  protected function get_record_class_name( $index )
+  protected function get_record_class_name( $index, $relative = false )
   {
     $subject = $this->get_subject( $index );
-    return 'surveys' == $subject ?
-      lib::get_class_name( 'database\limesurvey\surveys' ) : parent::get_record_class_name( $index );
+    if( is_null( $subject ) )
+      throw lib::create( 'exception\runtime',
+        sprintf( 'Tried to get record class name for invalid subject (index: %d)', $index ),
+        __METHOD__ );
+
+    $class = sprintf( 'database\limesurvey\surveys', $subject );
+    return $relative ? $class : lib::get_class_name( $class );
+
+    $subject = $this->get_subject( $index );
+    if( 'surveys' == $subject )
+    {
+      $class = 'database\limesurvey\surveys';
+      return $relative ? $class : lib::get_class_name( $class );
+    }
+
+    return parent::get_record_class_name( $index, $relative );
   }
 }
