69736739302aa666bd701daf3287789e0a881b8f
Patrick Emond <emondpd@mcmaster.ca>
2016-03-11 14:22:31 -0500

    Fixing bug in activity::close_lapsed()

--- src/database/activity.class.php
+++ src/database/activity.class.php
@@ -34,25 +34,28 @@ class activity extends record
   {
     $setting_manager = lib::create( 'business\setting_manager' );
 
-    static::db()->execute( sprintf(
-      'UPDATE activity'.
-      "\n".'JOIN access USING( user_id, role_id, site_id )'.
-      "\n".'SET end_datetime = datetime'.
-      "\n".'WHERE end_datetime IS NULL'.
-      "\n".'  AND datetime + INTERVAL %s MINUTE < UTC_TIMESTAMP()',
-      $setting_manager->get_setting( 'general', 'activity_timeout' ) ) );
+    if( !is_null( $db_user ) )
+    {
+      static::db()->execute( sprintf(
+        'UPDATE activity'.
+        "\n".'JOIN access USING( user_id, role_id, site_id )'.
+        "\n".'SET end_datetime = datetime'.
+        "\n".'WHERE end_datetime IS NULL'.
+        "\n".'  AND datetime + INTERVAL %s MINUTE < UTC_TIMESTAMP()',
+        $setting_manager->get_setting( 'general', 'activity_timeout' ) ) );
 
-    // close all open activity by this user NOT for the current site/role
-    $modifier = lib::create( 'database\modifier' );
-    $modifier->where( 'end_datetime', '=', NULL );
-    $modifier->where( 'user_id', '=', $db_user->id );
-    $modifier->where_bracket( true );
-    if( !is_null( $db_site ) ) $modifier->where( 'site_id', '!=', $db_site->id );
-    if( !is_null( $db_role ) ) $modifier->or_where( 'role_id', '!=', $db_role->id );
-    $modifier->where_bracket( false );
+      // close all open activity by this user NOT for the current site/role
+      $modifier = lib::create( 'database\modifier' );
+      $modifier->where( 'end_datetime', '=', NULL );
+      $modifier->where( 'user_id', '=', $db_user->id );
+      $modifier->where_bracket( true );
+      if( !is_null( $db_site ) ) $modifier->where( 'site_id', '!=', $db_site->id );
+      if( !is_null( $db_role ) ) $modifier->or_where( 'role_id', '!=', $db_role->id );
+      $modifier->where_bracket( false );
 
-    static::db()->execute( sprintf(
-      'UPDATE activity SET end_datetime = UTC_TIMESTAMP() %s',
-      $modifier->get_sql() ) );
+      static::db()->execute( sprintf(
+        'UPDATE activity SET end_datetime = UTC_TIMESTAMP() %s',
+        $modifier->get_sql() ) );
+    }
   }
 }
