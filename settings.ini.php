0f7536d9362568bc357d37e73f53c37033450180
Patrick Emond <emondpd@mcmaster.ca>
2016-03-30 16:49:53 -0400

    Bumping version and updating error codes

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.0.1';
-$settings['general']['cenozo_build'] = '672e75a';
+$settings['general']['cenozo_build'] = '2d46b29';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
