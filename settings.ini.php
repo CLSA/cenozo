098fa1066fcdf454b2395452b2c22dc9f950709b
Patrick Emond <emondpd@mcmaster.ca>
2016-05-06 12:07:23 -0400

    Speeding up alert bar pulsing speed

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.0.1';
-$settings['general']['cenozo_build'] = '6110d22';
+$settings['general']['cenozo_build'] = '4d50d90';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
