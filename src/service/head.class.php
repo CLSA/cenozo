66d81fe9badd79d22df3cfe453104f8f763dabf7
Patrick Emond <emondpd@mcmaster.ca>
2016-06-08 11:51:33 -0400

    Allowing for easier customization of service\head column header
    Moving some code in service\head::execute() to service\head::setup()

--- src/service/head.class.php
+++ src/service/head.class.php
@@ -25,6 +25,19 @@ class head extends read
   /**
    * Extends parent method
    */
+  protected function setup()
+  {
+    parent::setup();
+
+    // get the details of the leaf record
+    $util_class_name = lib::get_class_name( 'util' );
+    $record_class_name = $this->get_leaf_record_class_name();
+    $this->columns = $record_class_name::db()->get_column_details( $this->get_leaf_subject() );
+  }
+
+  /**
+   * Extends parent method
+   */
   protected function execute()
   {
     parent::execute();
@@ -32,7 +45,13 @@ class head extends read
     // get the details of the leaf record
     $util_class_name = lib::get_class_name( 'util' );
     $record_class_name = $this->get_leaf_record_class_name();
-    $this->headers['Columns'] = $util_class_name::json_encode(
-      $record_class_name::db()->get_column_details( $this->get_leaf_subject() ) );
+    $this->headers['Columns'] = $util_class_name::json_encode( $this->columns );
   }
+
+  /**
+   * The column details to be returned in a header
+   * @array $columns
+   * @access protected
+   */
+  protected $columns = NULL;
 }
