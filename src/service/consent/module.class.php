8621b7307df9c65e5ad11f9265b4aa73975a98da
Patrick Emond <emondpd@mcmaster.ca>
2016-07-19 10:22:49 -0400

    Testing consent and event records for application and site restrictions

--- src/service/consent/module.class.php
+++ src/service/consent/module.class.php
@@ -12,7 +12,7 @@ use cenozo\lib, cenozo\log;
 /**
  * Performs operations which effect how this module is used in a service
  */
-class module extends \cenozo\service\module
+class module extends \cenozo\service\site_restricted_module
 {
   /**
    * Extend parent method
@@ -26,12 +26,70 @@ class module extends \cenozo\service\module
       // make sure the application has access to the participant
       $db_application = lib::create( 'business\session' )->get_application();
       $db_consent = $this->get_resource();
-      if( $db_application->release_based && !is_null( $db_consent ) )
+      if( !is_null( $db_consent ) )
       {
-        $modifier = lib::create( 'database\modifier' );
-        $modifier->where( 'participant_id', '=', $db_consent->participant_id );
-        if( 0 == $db_application->get_participant_count( $modifier ) ) $this->get_status()->set_code( 404 );
+        if( $db_application->release_based )
+        {
+          $modifier = lib::create( 'database\modifier' );
+          $modifier->where( 'participant_id', '=', $db_consent->participant_id );
+          if( 0 == $db_application->get_participant_count( $modifier ) ) $this->get_status()->set_code( 404 );
+        }
+
+        // make sure the application has access to the participant
+        $db_application = lib::create( 'business\session' )->get_application();
+        if( $db_application->release_based )
+        {
+          $modifier = lib::create( 'database\modifier' );
+          $modifier->where( 'participant_id', '=', $db_consent->participant_id );
+          if( 0 == $db_application->get_participant_count( $modifier ) )
+          {
+            $this->get_status()->set_code( 404 );
+            return;
+          }
+        }
+
+        // restrict by site
+        $db_restrict_site = $this->get_restricted_site();
+        if( !is_null( $db_restrict_site ) )
+        {
+          if( $db_consent->get_participant()->get_effective_site()->id != $db_restrict_site->id )
+          {
+            $this->get_status()->set_code( 403 );
+            return;
+          }
+        }
       }
+    }
+  }
+
+  /**
+   * Extend parent method
+   */
+  public function prepare_read( $select, $modifier )
+  {
+    parent::prepare_read( $select, $modifier );
+
+    $db_application = lib::create( 'business\session' )->get_application();
+
+    // restrict to participants in this application
+    if( $db_application->release_based )
+    {
+      $sub_mod = lib::create( 'database\modifier' );
+      $sub_mod->where( 'consent.participant_id', '=', 'application_has_participant.participant_id', false );
+      $sub_mod->where( 'application_has_participant.application_id', '=', $db_application->id );
+      $sub_mod->where( 'application_has_participant.datetime', '!=', NULL );
+      $modifier->join_modifier( 'application_has_participant', $sub_mod );
+    }
+
+    // restrict by site
+    $db_restrict_site = $this->get_restricted_site();
+    if( !is_null( $db_restrict_site ) )
+    {
+      $sub_mod = lib::create( 'database\modifier' );
+      $sub_mod->where( 'consent.participant_id', '=', 'participant_site.participant_id', false );
+      $sub_mod->where( 'participant_site.application_id', '=', $db_application->id );
+      $sub_mod->where( 'participant_site.site_id', '=', $db_restrict_site->id );
+      $modifier->join_modifier( 'participant_site', $sub_mod );
     }
   }
 }
--- src/service/event/module.class.php
+++ src/service/event/module.class.php
@@ -12,7 +12,7 @@ use cenozo\lib, cenozo\log;
 /**
  * Performs operations which effect how this module is used in a service
  */
-class module extends \cenozo\service\module
+class module extends \cenozo\service\site_restricted_module
 {
   /**
    * Extend parent method
@@ -26,11 +26,38 @@ class module extends \cenozo\service\module
       // make sure the application has access to the participant
       $db_application = lib::create( 'business\session' )->get_application();
       $db_event = $this->get_resource();
-      if( $db_application->release_based && !is_null( $db_event ) )
+      if( !is_null( $db_event ) )
       {
-        $modifier = lib::create( 'database\modifier' );
-        $modifier->where( 'participant_id', '=', $db_event->participant_id );
-        if( 0 == $db_application->get_participant_count( $modifier ) ) $this->get_status()->set_code( 404 );
+        if( $db_application->release_based )
+        {
+          $modifier = lib::create( 'database\modifier' );
+          $modifier->where( 'participant_id', '=', $db_event->participant_id );
+          if( 0 == $db_application->get_participant_count( $modifier ) ) $this->get_status()->set_code( 404 );
+        }
+
+        // make sure the application has access to the participant
+        $db_application = lib::create( 'business\session' )->get_application();
+        if( $db_application->release_based )
+        {
+          $modifier = lib::create( 'database\modifier' );
+          $modifier->where( 'participant_id', '=', $db_event->participant_id );
+          if( 0 == $db_application->get_participant_count( $modifier ) )
+          {
+            $this->get_status()->set_code( 404 );
+            return;
+          }
+        }
+
+        // restrict by site
+        $db_restrict_site = $this->get_restricted_site();
+        if( !is_null( $db_restrict_site ) )
+        {
+          if( $db_event->get_participant()->get_effective_site()->id != $db_restrict_site->id )
+          {
+            $this->get_status()->set_code( 403 );
+            return;
+          }
+        }
       }
     }
   }
@@ -41,6 +68,29 @@ class module extends \cenozo\service\module
   public function prepare_read( $select, $modifier )
   {
     parent::prepare_read( $select, $modifier );
+
+    $db_application = lib::create( 'business\session' )->get_application();
+
+    // restrict to participants in this application
+    if( $db_application->release_based )
+    {
+      $sub_mod = lib::create( 'database\modifier' );
+      $sub_mod->where( 'event.participant_id', '=', 'application_has_participant.participant_id', false );
+      $sub_mod->where( 'application_has_participant.application_id', '=', $db_application->id );
+      $sub_mod->where( 'application_has_participant.datetime', '!=', NULL );
+      $modifier->join_modifier( 'application_has_participant', $sub_mod );
+    }
+
+    // restrict by site
+    $db_restrict_site = $this->get_restricted_site();
+    if( !is_null( $db_restrict_site ) )
+    {
+      $sub_mod = lib::create( 'database\modifier' );
+      $sub_mod->where( 'event.participant_id', '=', 'participant_site.participant_id', false );
+      $sub_mod->where( 'participant_site.application_id', '=', $db_application->id );
+      $sub_mod->where( 'participant_site.site_id', '=', $db_restrict_site->id );
+      $modifier->join_modifier( 'participant_site', $sub_mod );
+    }
 
     if( $select->has_table_columns( 'site' ) ) $modifier->left_join( 'site', 'event.site_id', 'site.id' );
     if( $select->has_table_columns( 'user' ) ) $modifier->left_join( 'user', 'event.user_id', 'user.id' );
