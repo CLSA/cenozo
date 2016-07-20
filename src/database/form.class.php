93787355dae1f68bbb0a351859aea601bea281e3
Patrick Emond <emondpd@mcmaster.ca>
2016-07-20 11:23:14 -0400

    Working on form module

--- src/database/form.class.php
+++ src/database/form.class.php
@@ -24,8 +24,9 @@ class form extends record
     }
 
     $padded_id = str_pad( $this->id, 7, '0', STR_PAD_LEFT );
-    $filename = sprintf( '%s/%s/%s/%s.pdf',
-                         sprintf( '%s/%s', FORM_PATH, $this->get_form_type()->name ),
+    $filename = sprintf( '%s/%s/%s/%s/%s.pdf',
+                         FORM_PATH,
+                         $this->get_form_type()->name,
                          substr( $padded_id, 0, 3 ),
                          substr( $padded_id, 3, 2 ),
                          substr( $padded_id, 5 ) );
