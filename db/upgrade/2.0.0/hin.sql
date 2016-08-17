fec69119e9f17e06b40b68ffc861e4f44f529c9c
Patrick Emond <emondpd@mcmaster.ca>
2016-08-17 11:59:55 -0400

    Adding missing code to database upgrade script

--- db/upgrade/2.0.0/hin.sql
+++ db/upgrade/2.0.0/hin.sql
@@ -41,6 +41,18 @@ CREATE PROCEDURE patch_hin()
       ALTER TABLE hin DROP COLUMN future_access;
     END IF;
 
+    SELECT "Removing access column from hin table" AS "";
+
+    SET @test = (
+      SELECT COUNT(*)
+      FROM information_schema.COLUMNS
+      WHERE TABLE_SCHEMA = DATABASE()
+      AND TABLE_NAME = "hin"
+      AND COLUMN_NAME = "access" );
+    IF @test = 1 THEN
+      ALTER TABLE hin DROP COLUMN access;
+    END IF;
+
     SELECT "Making hin.code column mandatory" AS "";
 
     SET @test = (
