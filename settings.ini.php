1114d42d916dba70a0a9c94a91345b4b67873b64
Patrick Emond <emondpd@mcmaster.ca>
2016-09-07 17:13:10 -0400

    Adding new isIncluded option to module column list items

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.0.1';
-$settings['general']['cenozo_build'] = '7bd8d74';
+$settings['general']['cenozo_build'] = 'c1ddc30';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
