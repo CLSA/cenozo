072d66aa858340cf417f287684cdc4ae57f7bead
Patrick Emond <emondpd@mcmaster.ca>
2016-09-21 13:54:54 -0400

    Adding calendar-event-cancelled class to cenozo.css

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.0.1';
-$settings['general']['cenozo_build'] = '765a383';
+$settings['general']['cenozo_build'] = '620d324';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
