a31abb35a981f59f50413b8292688412e712a725
Patrick Emond <emondpd@mcmaster.ca>
2016-10-12 17:36:21 -0400

    Adding overview.name column to cenozo schema

--- db/upgrade/2.0.0/overview.sql
+++ db/upgrade/2.0.0/overview.sql
@@ -4,8 +4,9 @@ CREATE TABLE IF NOT EXISTS overview (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   update_timestamp TIMESTAMP NOT NULL,
   create_timestamp TIMESTAMP NOT NULL,
+  name VARCHAR(45) NOT NULL,
   title VARCHAR(255) NOT NULL,
   description TEXT NULL,
   PRIMARY KEY (id),
-  UNIQUE INDEX uq_title (title ASC))
+  UNIQUE INDEX uq_name (name ASC))
 ENGINE = InnoDB;
