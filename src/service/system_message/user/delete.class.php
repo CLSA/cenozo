e187c34b1f90660f2c6ca7bffd8bd2ce66b4c59e
Patrick Emond <emondpd@mcmaster.ca>
2016-11-02 17:16:18 -0400

    Fixing bug in last commit

--- src/service/system_message/user/delete.class.php
+++ src/service/system_message/user/delete.class.php
@@ -24,12 +24,12 @@ class delete extends \cenozo\service\delete
     $db_user = $this->get_resource( 1 );
     $self = $db_user->id == lib::create( 'business\session' )->get_user()->id;
 
-    if( 300 >= $this->status->get_code() )
+    if( 300 > $this->status->get_code() )
     {
       // don't allow users to mark other user's messages as unread
       if( !$self ) $this->status->set_code( 403 );
     }
-    else if( 403 == $this->status->get_code() )
+    else if( 306 == $this->status->get_code() || 403 == $this->status->get_code() )
     {
       // allow anyone to makr their own system messages as unread
       if( $self ) $this->status->set_code( 200 );
--- src/service/system_message/user/post.class.php
+++ src/service/system_message/user/post.class.php
@@ -24,12 +24,12 @@ class post extends \cenozo\service\post
     $user_id = $this->get_file_as_raw();
     $self = $user_id == lib::create( 'business\session' )->get_user()->id;
 
-    if( 300 >= $this->status->get_code() )
+    if( 300 > $this->status->get_code() )
     {
       // don't allow users to mark other user's messages as read
       if( !$self ) $this->status->set_code( 403 );
     }
-    else if( 403 == $this->status->get_code() )
+    else if( 306 == $this->status->get_code() || 403 == $this->status->get_code() )
     {
       // allow anyone to makr their own system messages as read
       if( $self ) $this->status->set_code( 200 );
