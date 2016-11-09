a64567e2c74e9847a1b6c129fdc2bb031b97850f
Patrick Emond <emondpd@mcmaster.ca>
2016-11-09 15:09:27 -0500

    Fixing bug in notes (could not select note text in firefox)

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.0.1';
-$settings['general']['cenozo_build'] = '79b1e17';
+$settings['general']['cenozo_build'] = '3070b7d';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
