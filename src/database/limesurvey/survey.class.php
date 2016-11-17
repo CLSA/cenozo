beb9d66df16136f6f67633bae92d5a8c67f35911
Patrick Emond <emondpd@mcmaster.ca>
2016-11-17 16:21:08 -0500

    Creating database\limesurvey\survey::get_column_name_for_question_code() method

--- src/database/limesurvey/survey.class.php
+++ src/database/limesurvey/survey.class.php
@@ -53,28 +53,10 @@ class survey extends sid_record
    */
   public function get_response( $question_code )
   {
-    $select = lib::create( 'database\select' );
-    $select->add_column( 'gid' );
-    $select->add_column( 'qid' );
-    $select->add_column( 'parent_qid' );
-    $select->from( 'questions' );
-
-    // the questions table has more than one column in its primary key so custom sql is needed
-    $modifier = lib::create( 'database\modifier' );
-    $modifier->where( 'sid', '=', static::get_sid() );
-    $modifier->where( 'title', '=', $question_code );
-    $modifier->group( 'sid' );
-    $modifier->group( 'gid' );
-    $modifier->group( 'qid' );
-    $sql = sprintf( '%s %s', $select->get_sql(), $modifier->get_sql() );
-
-    $row = static::db()->get_row( $sql );
-    if( 0 == count( $row ) )
+    $column_name = static::get_column_name_for_question_code( $question_code );
+    if( is_null( $column_name ) )
       throw lib::create( 'exception\runtime', 'Question code not found in survey.', __METHOD__ );
 
-    $column_name = $row['parent_qid']
-                 ? sprintf( '%sX%sX%s%s', static::get_sid(), $row['gid'], $row['parent_qid'], $question_code )
-                 : sprintf( '%sX%sX%s', static::get_sid(), $row['gid'], $row['qid'] );
     return $this->$column_name;
   }
 
@@ -89,30 +71,12 @@ class survey extends sid_record
    */
   public static function get_responses( $question_code, $modifier = NULL )
   {
-    $question_sel = lib::create( 'database\select' );
-    $question_sel->add_column( 'gid' );
-    $question_sel->add_column( 'qid' );
-    $select->add_column( 'parent_qid' );
-    $question_sel->from( 'questions' );
-
-    // the questions table has more than one column in its primary key so custom sql is needed
-    $question_mod = lib::create( 'database\modifier' );
-    $question_mod->where( 'sid', '=', static::get_sid() );
-    $question_mod->where( 'title', '=', $question_code );
-    $question_mod->group( 'sid' );
-    $question_mod->group( 'gid' );
-    $question_mod->group( 'qid' );
-    $sql = sprintf( '%s %s', $question_sel->get_sql(), $question_mod->get_sql() );
-
-    $row = static::db()->get_row( $sql );
-    if( 0 == count( $row ) )
+    $column_name = static::get_column_name_for_question_code( $question_code );
+    if( is_null( $column_name ) )
       throw lib::create( 'exception\runtime', 'Question code not found in survey.', __METHOD__ );
 
     $select = lib::create( 'database\select' );
-    $select->add_column(
-      $row['parent_qid'] ?
-      sprintf( '%sX%sX%s%s', static::get_sid(), $row['gid'], $row['parent_qid'], $question_code ) :
-      sprintf( '%sX%sX%s', static::get_sid(), $row['gid'], $row['qid'] ) );
+    $select->add_column( $column_name );
     $select->from( static::get_table_name() );
 
     $sql = $select->get_sql();
@@ -141,6 +105,45 @@ class survey extends sid_record
     $modifier->join( $timing_table_name, $table_name.'.id', $timing_table_name.'.id' );
 
     return static::db()->get_one( sprintf( '%s %s', $select->get_sql(), $modifier->get_sql() ) );
+  }
+
+  /**
+   * Returns the table's column name for the given question code
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param string $question_code
+   * @return string
+   * @access public
+   * @static
+   */
+  public static function get_column_name_for_question_code( $question_code )
+  {
+    $column_name = NULL;
+
+    $select = lib::create( 'database\select' );
+    $select->add_column( 'gid' );
+    $select->add_column( 'qid' );
+    $select->add_column( 'parent_qid' );
+    $select->from( 'questions' );
+
+    // the questions table has more than one column in its primary key so custom sql is needed
+    $modifier = lib::create( 'database\modifier' );
+    $modifier->where( 'sid', '=', static::get_sid() );
+    $modifier->where( 'title', '=', $question_code );
+    $modifier->group( 'sid' );
+    $modifier->group( 'gid' );
+    $modifier->group( 'qid' );
+    $sql = sprintf( '%s %s', $select->get_sql(), $modifier->get_sql() );
+
+    $row = static::db()->get_row( $sql );
+    if( 0 < count( $row ) )
+    {
+      $column_name = $row['parent_qid']
+                   ? sprintf( '%sX%sX%s%s', static::get_sid(), $row['gid'], $row['parent_qid'], $question_code )
+                   : sprintf( '%sX%sX%s', static::get_sid(), $row['gid'], $row['qid'] );
+    }
+
+    return $column_name;
   }
 
   /**
