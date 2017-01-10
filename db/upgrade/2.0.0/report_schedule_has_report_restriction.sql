c59c707ebcbae6f82ab8a6717de2a002d81dc548
Patrick Emond <emondpd@mcmaster.ca>
2017-01-10 13:53:38 -0500

    Fixing database schema bug
    report_schedule_has_restriction.value was VARCHAR(45) instead of TEXT

--- db/upgrade/2.0.0/report_schedule_has_report_restriction.sql
+++ db/upgrade/2.0.0/report_schedule_has_report_restriction.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS report_schedule_has_report_restriction (
   report_restriction_id INT UNSIGNED NOT NULL,
   update_timestamp TIMESTAMP NOT NULL,
   create_timestamp TIMESTAMP NOT NULL,
-  value VARCHAR(45) NOT NULL,
+  value TEXT NOT NULL,
   PRIMARY KEY (report_schedule_id, report_restriction_id),
   INDEX fk_report_restriction_id (report_restriction_id ASC),
   INDEX fk_report_schedule_id (report_schedule_id ASC),
