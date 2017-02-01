b2bb1b3e6c2f5953b6e4993da294fef495b93693
Patrick Emond <emondpd@mcmaster.ca>
2017-02-01 11:42:05 -0500

    Compressing participant notes and history

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.0.1';
-$settings['general']['cenozo_build'] = '097b818';
+$settings['general']['cenozo_build'] = '389ce5c';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
