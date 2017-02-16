4233171a79ac3e48f8a2feffaf69891fe461260f
Patrick Emond <emondpd@mcmaster.ca>
2017-02-16 15:04:18 -0500

    Forgot to bump version

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.1.0';
-$settings['general']['cenozo_build'] = '2bbd45b';
+$settings['general']['cenozo_build'] = '92251b5';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
