11b6ce4627332e4fd5b2eec3fdb130129311cf5d
Patrick Emond <emondpd@mcmaster.ca>
2017-04-12 11:23:57 -0400

    Replacing typeahead with uib-typeahead

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.1.0';
-$settings['general']['cenozo_build'] = 'cf4cf9a';
+$settings['general']['cenozo_build'] = 'a751282';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
