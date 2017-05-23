d09143b09b23933de37347e1a6e27fb6c21c75e2
Patrick Emond <emondpd@mcmaster.ca>
2017-05-23 15:46:44 -0400

    Restricting table-based report restricts to those tables that exist in the application

--- src/service/report_restriction/module.class.php
+++ src/service/report_restriction/module.class.php
@@ -30,11 +30,23 @@ class module extends \cenozo\service\module
       $modifier->where_bracket( false );
     }
 
+    // don't show restrictions with subjects that don't exist in this application
+    $table_list = lib::create( 'business\session' )->get_database()->get_table_names();
+    $modifier->where_bracket( true );
+    $modifier->where( 'report_restriction.restriction_type', '!=', 'table' );
+    $modifier->or_where( 'report_restriction.subject', '=', NULL );
+    $modifier->or_where( 'report_restriction.subject', 'IN', $table_list );
+    $modifier->where_bracket( false );
+
     // if the parent is a report then add the value
     if( 'report' == $this->get_parent_subject() )
       $select->add_table_column( 'report_has_report_restriction', 'value' );
     // if the parent is a report_schedule then add the value
     else if( 'report_schedule' == $this->get_parent_subject() )
       $select->add_table_column( 'report_schedule_has_report_restriction', 'value' );
+
+    // add the report type's subject as "table"
+    $modifier->join( 'report_type', 'report_restriction.report_type_id', 'report_type.id' );
+    $select->add_table_column( 'report_type', 'subject', 'base_table' );
   }
 }
