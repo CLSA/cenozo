366782dbc3ff342cd9ffadf5077b03c815aab80e
Patrick Emond <emondpd@mcmaster.ca>
2017-06-16 10:49:54 -0400

    Removing invalid "View Callback List" button from callback calendar

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.1.0';
-$settings['general']['cenozo_build'] = 'aae12da';
+$settings['general']['cenozo_build'] = 'bded6d2';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
