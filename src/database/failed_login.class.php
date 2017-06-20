d18ce992e8ff776a3212c8652eac48babe517e13
Patrick Emond <emondpd@mcmaster.ca>
2017-06-20 14:53:14 -0400

    Adding a new feature to track failed logins

---
+++ src/database/failed_login.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * failed_login.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ * @filesource
+ */
+
+namespace cenozo\database;
+use cenozo\lib, cenozo\log;
+
+/**
+ * failed_login: record
+ */
+class failed_login extends record {}
