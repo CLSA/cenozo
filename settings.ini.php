e141bebfe33b0769882d21a6a8ca6dd4d22f692a
Patrick Emond <emondpd@mcmaster.ca>
2017-06-23 09:57:42 -0400

    Updating patch version

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.1.0';
-$settings['general']['cenozo_build'] = '7d023e3';
+$settings['general']['cenozo_build'] = '33d5b67';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
