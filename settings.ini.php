266dc0162ba7257c1e34a17a300b478f2ea66e17
Patrick Emond <emondpd@mcmaster.ca>
2017-10-17 17:47:46 -0400

    Moving "opal forms" buttom from cenozo's UI to mastodon's UI

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.1.0';
-$settings['general']['cenozo_build'] = 'b59fffd';
+$settings['general']['cenozo_build'] = 'ec667ad';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
