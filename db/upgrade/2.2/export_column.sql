6dace3f9d2686a951ba3bbb30a175e15944ca70a
Patrick Emond <emondpd@mcmaster.ca>
2018-01-18 15:32:27 -0500

    Fixing db update script bugs

--- db/upgrade/2.2/export_column.sql
+++ db/upgrade/2.2/export_column.sql
@@ -1,34 +1,54 @@
-SELECT "Converting export_columns from state to hold/proxy/trace" AS "";
+DROP PROCEDURE IF EXISTS patch_export_column;
+DELIMITER //
+CREATE PROCEDURE patch_export_column()
+  BEGIN
 
-DELETE FROM export_column
-WHERE table_name = "participant"
-AND column_name = "active";
+    SET @test = ( 
+      SELECT COUNT(*)
+      FROM information_schema.TABLES
+      WHERE TABLE_SCHEMA = DATABASE()
+      AND TABLE_NAME = "state" );
+    IF @test = 1 THEN
 
-UPDATE export_column
-JOIN export_restriction USING( export_id )
-SET export_column.table_name = "hold", export_column.column_name = "hold_type_id"
-WHERE export_column.table_name = "participant"
-AND export_column.column_name = "state_id"
-AND export_restriction.table_name = "hold"
-AND export_restriction.column_name = "hold_type_id";
+      SELECT "Converting export_columns from state to hold/proxy/trace" AS "";
 
-UPDATE export_column
-JOIN export_restriction USING( export_id )
-SET export_column.table_name = "proxy", export_column.column_name = "proxy_type_id"
-WHERE export_column.table_name = "participant"
-AND export_column.column_name = "state_id"
-AND export_restriction.table_name = "proxy"
-AND export_restriction.column_name = "proxy_type_id";
+      DELETE FROM export_column
+      WHERE table_name = "participant"
+      AND column_name = "active";
 
-UPDATE export_column
-JOIN export_restriction USING( export_id )
-SET export_column.table_name = "trace", export_column.column_name = "trace_type_id"
-WHERE export_column.table_name = "participant"
-AND export_column.column_name = "state_id"
-AND export_restriction.table_name = "trace"
-AND export_restriction.column_name = "trace_type_id";
+      UPDATE export_column
+      JOIN export_restriction USING( export_id )
+      SET export_column.table_name = "hold", export_column.column_name = "hold_type_id"
+      WHERE export_column.table_name = "participant"
+      AND export_column.column_name = "state_id"
+      AND export_restriction.table_name = "hold"
+      AND export_restriction.column_name = "hold_type_id";
 
-UPDATE export_column
-SET column_name = "status"
-WHERE table_name = "participant"
-AND column_name = "state_id";
+      UPDATE export_column
+      JOIN export_restriction USING( export_id )
+      SET export_column.table_name = "proxy", export_column.column_name = "proxy_type_id"
+      WHERE export_column.table_name = "participant"
+      AND export_column.column_name = "state_id"
+      AND export_restriction.table_name = "proxy"
+      AND export_restriction.column_name = "proxy_type_id";
+
+      UPDATE export_column
+      JOIN export_restriction USING( export_id )
+      SET export_column.table_name = "trace", export_column.column_name = "trace_type_id"
+      WHERE export_column.table_name = "participant"
+      AND export_column.column_name = "state_id"
+      AND export_restriction.table_name = "trace"
+      AND export_restriction.column_name = "trace_type_id";
+
+      UPDATE export_column
+      SET column_name = "status"
+      WHERE table_name = "participant"
+      AND column_name = "state_id";
+
+    END IF;
+
+  END //
+DELIMITER ;
+
+CALL patch_export_column();
+DROP PROCEDURE IF EXISTS patch_export_column;
--- db/upgrade/2.2/export_restriction.sql
+++ db/upgrade/2.2/export_restriction.sql
@@ -1,46 +1,66 @@
-SELECT "Converting export_restrictions from state to hold/proxy/trace" AS "";
+DROP PROCEDURE IF EXISTS patch_export_restriction;
+DELIMITER //
+CREATE PROCEDURE patch_export_restriction()
+  BEGIN
 
--- convert state is null to hold is null
-UPDATE export_restriction
-SET table_name = "hold", column_name = "hold_type_id"
-WHERE table_name = "participant"
-AND column_name = "state_id"
-AND value IS NULL;
+    SET @test = ( 
+      SELECT COUNT(*)
+      FROM information_schema.TABLES
+      WHERE TABLE_SCHEMA = DATABASE()
+      AND TABLE_NAME = "state" );
+    IF @test = 1 THEN
 
--- convert state is hold-type to hold is hold-type
-UPDATE export_restriction
-JOIN state ON export_restriction.value = state.id
-JOIN hold_type ON state.name = hold_type.name
-SET table_name = "hold", column_name = "hold_type_id", value = hold_type.id
-WHERE table_name = "participant"
-AND column_name = "state_id";
+      SELECT "Converting export_restrictions from state to hold/proxy/trace" AS "";
 
--- convert participant.active = 0 to hold is hold-type
-UPDATE export_restriction, hold_type
-SET table_name = "hold", column_name = "hold_type_id", value = hold_type.id
-WHERE table_name = "participant"
-AND column_name = "active"
-AND value = 1
-AND hold_type.name = "Deactivated";
+      -- convert state is null to hold is null
+      UPDATE export_restriction
+      SET table_name = "hold", column_name = "hold_type_id"
+      WHERE table_name = "participant"
+      AND column_name = "state_id"
+      AND value IS NULL;
 
--- delete participant.active = 1
-DELETE FROM export_restriction
-WHERE table_name = "participant"
-AND column_name = "active"
-AND value = 0;
+      -- convert state is hold-type to hold is hold-type
+      UPDATE export_restriction
+      JOIN state ON export_restriction.value = state.id
+      JOIN hold_type ON state.name = hold_type.name
+      SET table_name = "hold", column_name = "hold_type_id", value = hold_type.id
+      WHERE table_name = "participant"
+      AND column_name = "state_id";
 
--- convert state is proxy-type to proxy is proxy-type
-UPDATE export_restriction
-JOIN state ON export_restriction.value = state.id
-JOIN proxy_type ON state.name = proxy_type.name
-SET table_name = "proxy", column_name = "proxy_type_id", value = proxy_type.id
-WHERE table_name = "participant"
-AND column_name = "state_id";
+      -- convert participant.active = 0 to hold is hold-type
+      UPDATE export_restriction, hold_type
+      SET table_name = "hold", column_name = "hold_type_id", value = hold_type.id
+      WHERE table_name = "participant"
+      AND column_name = "active"
+      AND value = 1
+      AND hold_type.name = "Deactivated";
 
--- convert state is trace-type to trace is trace-type
-UPDATE export_restriction
-JOIN state ON export_restriction.value = state.id
-JOIN trace_type ON state.name = trace_type.name
-SET table_name = "trace", column_name = "trace_type_id", value = trace_type.id
-WHERE table_name = "participant"
-AND column_name = "state_id";
+      -- delete participant.active = 1
+      DELETE FROM export_restriction
+      WHERE table_name = "participant"
+      AND column_name = "active"
+      AND value = 0;
+
+      -- convert state is proxy-type to proxy is proxy-type
+      UPDATE export_restriction
+      JOIN state ON export_restriction.value = state.id
+      JOIN proxy_type ON state.name = proxy_type.name
+      SET table_name = "proxy", column_name = "proxy_type_id", value = proxy_type.id
+      WHERE table_name = "participant"
+      AND column_name = "state_id";
+
+      -- convert state is trace-type to trace is trace-type
+      UPDATE export_restriction
+      JOIN state ON export_restriction.value = state.id
+      JOIN trace_type ON state.name = trace_type.name
+      SET table_name = "trace", column_name = "trace_type_id", value = trace_type.id
+      WHERE table_name = "participant"
+      AND column_name = "state_id";
+
+    END IF;
+
+  END //
+DELIMITER ;
+
+CALL patch_export_restriction();
+DROP PROCEDURE IF EXISTS patch_export_restriction;
