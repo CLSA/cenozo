6397f6f9c3f728bda5bf067971c357746370f3f2
Patrick Emond <emondpd@mcmaster.ca>
2018-03-26 17:11:14 -0400

    Minor tweaks to view-input's action button

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.2';
-$settings['general']['cenozo_build'] = 'cc630c6';
+$settings['general']['cenozo_build'] = '4eba7c0';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
