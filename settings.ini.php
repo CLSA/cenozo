106bf0af301f584e5789c051d917f519d28a2291
Patrick Emond <emondpd@mcmaster.ca>
2018-05-02 14:51:52 -0400

    Adding styling to <sup> in cenozo.css

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.2';
-$settings['general']['cenozo_build'] = 'ee2e123';
+$settings['general']['cenozo_build'] = '5bea9e0';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
