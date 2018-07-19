6581dc436c7ae561d2a44e96c6438b7caf3f272d
Patrick Emond <emondpd@mcmaster.ca>
2018-07-19 13:29:34 -0400

    Fixing bug in extra-operation-button-group template which wasn't disabling items properly

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.2';
-$settings['general']['cenozo_build'] = 'e0cf53c';
+$settings['general']['cenozo_build'] = '03059b0';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
