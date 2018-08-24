9d584273b5e0d2e10bfad5ae44f168679be01e04
Patrick Emond <emondpd@mcmaster.ca>
2018-08-24 14:54:11 -0400

    Changing css font-size for sub to 110%

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.2';
-$settings['general']['cenozo_build'] = 'd798f15';
+$settings['general']['cenozo_build'] = 'd9deb00';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
