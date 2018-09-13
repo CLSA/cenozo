98d905facae0b6e35a88f9eea613c7af10c41876
Patrick Emond <emondpd@mcmaster.ca>
2018-09-13 16:38:03 -0400

    Replacing the small branding logo with the standard one

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.2';
-$settings['general']['cenozo_build'] = 'e402cad';
+$settings['general']['cenozo_build'] = 'f81519e';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
