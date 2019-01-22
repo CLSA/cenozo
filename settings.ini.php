b754267458340eabffdcd38f8d604107c296228c
Patrick Emond <emondpd@mcmaster.ca>
2019-01-22 13:49:57 -0500

    Granting curators access to making decedent responders

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.3';
-$settings['general']['cenozo_build'] = '51dbb8c';
+$settings['general']['cenozo_build'] = '988a7ab';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
