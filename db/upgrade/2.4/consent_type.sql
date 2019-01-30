5ab8b16058d62768e083b4b07e26e872f340e979
Patrick Emond <emondpd@mcmaster.ca>
2019-01-30 10:44:12 -0500

    Adding missing sql patch log text

--- db/upgrade/2.4/consent_type.sql
+++ db/upgrade/2.4/consent_type.sql
@@ -1,3 +1,5 @@
+SELECT "Creating new consent types" AS "";
+
 INSERT IGNORE INTO consent_type( name, description ) VALUES
 ( "Extended HIN Access", "Consent to grant 10 year extended linkage access to the participant's Health Insurance Number data." ),
 ( "CIHI Access", "Consent to grant linkage access to the participant's Canadian Institute for Health Information data." ),
