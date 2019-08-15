f4a472212438c18c445857561af47f399a11db4e
Patrick Emond <emondpd@mcmaster.ca>
2019-08-15 11:47:13 -0400

    Tweaking plot markup

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.4';
-$settings['general']['cenozo_build'] = 'd38de0e';
+$settings['general']['cenozo_build'] = '129f19e';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
