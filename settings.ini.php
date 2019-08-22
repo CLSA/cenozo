2e868058d8683fd97be67179314ec6a66096037b
Patrick Emond <emondpd@mcmaster.ca>
2019-08-22 11:03:11 -0400

    Need to update version for updated ui-router lib

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.4';
-$settings['general']['cenozo_build'] = '4c0fb68';
+$settings['general']['cenozo_build'] = '18e8ed2';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
