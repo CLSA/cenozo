69ec0c3e1ee8682b2da27610fad2a525e5a53e70
Patrick Emond <emondpd@mcmaster.ca>
2019-09-25 11:03:46 -0400

    Fixing bug where Firefox wasn't downloading files correctly
    It seems that having href="#" in the <a> element was confusing Firefox
    so it has been removed and replaced with a styling element so that the
    link still "acts" like a hyperlink should.

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.5';
-$settings['general']['cenozo_build'] = '443b33a';
+$settings['general']['cenozo_build'] = 'c941d95';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
