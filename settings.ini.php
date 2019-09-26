78b7be57c725b1471b5e1812d65fce97824a6342
Patrick Emond <emondpd@mcmaster.ca>
2019-09-26 13:26:16 -0400

    Binding extra operation button html text

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.5';
-$settings['general']['cenozo_build'] = 'c941d95';
+$settings['general']['cenozo_build'] = '55df18b';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
