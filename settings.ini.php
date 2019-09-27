68faf634c5426f7396a5bdd27aafa277404651a7
Patrick Emond <emondpd@mcmaster.ca>
2019-09-27 11:22:30 -0400

    Fixing UI bugs in last commit

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.5';
-$settings['general']['cenozo_build'] = '55df18b';
+$settings['general']['cenozo_build'] = '78b7be5';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
