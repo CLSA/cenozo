af9d0c0336f2c3cd1fc707b51d4df0d8b2ecfd5c
Patrick Emond <emondpd@mcmaster.ca>
2020-04-06 11:12:30 -0400

    Adding max-width-full css class

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.5';
-$settings['general']['cenozo_build'] = '1bebd0a';
+$settings['general']['cenozo_build'] = 'bf648d5';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
