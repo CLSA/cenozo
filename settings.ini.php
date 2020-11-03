550e441b65425aa76b5339d092a5001ad0f30912
Patrick Emond <emondpd@mcmaster.ca>
2020-11-03 09:26:07 -0500

    Updating build number since we seem to be having caching issues

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.6';
-$settings['general']['cenozo_build'] = '030a03f';
+$settings['general']['cenozo_build'] = 'd169d98';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
