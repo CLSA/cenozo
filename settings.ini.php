f2c6594d6c08633e6698671cfe3e69b4234fdc69
Patrick Emond <emondpd@mcmaster.ca>
2021-04-16 16:53:31 -0400

    Fixing bug in study UI module

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.7';
-$settings['general']['cenozo_build'] = '373d1773';
+$settings['general']['cenozo_build'] = 'a91b8fa1';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
