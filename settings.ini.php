97f42409e9f1f94ee27572b4bdcb82ed924f25fb
Patrick Emond <emondpd@mcmaster.ca>
2021-04-23 15:51:54 -0400

    Adding cnIgnoreMouseWheel directive

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.7';
-$settings['general']['cenozo_build'] = 'a91b8fa1';
+$settings['general']['cenozo_build'] = 'd1e35164';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
