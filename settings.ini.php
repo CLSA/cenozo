f3a6fa4ecb4f14cdb045235bb4bc96a8eecf672b
Patrick Emond <patrickdemond@gmail.com>
2022-05-22 14:20:49 -0400

    Converting for Pine-based decedent qnaire

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.8';
-$settings['general']['cenozo_build'] = '8a000a6b';
+$settings['general']['cenozo_build'] = 'c3879e3b';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
