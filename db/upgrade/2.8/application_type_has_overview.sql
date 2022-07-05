464ae048c3d25b51c06c35768f820a0e59874efd
Patrick Emond <emondpd@mcmaster.ca>
2022-07-05 10:20:49 -0400

    Fixing bug in db patch

--- db/upgrade/2.8/application_type_has_overview.sql
+++ db/upgrade/2.8/application_type_has_overview.sql
@@ -1,6 +1,6 @@
 SELECT "Re-enabling withdraw overview" AS "";
 
-INSERT INTO application_type_has_overview( application_type_id, overview_id )
+INSERT IGNORE INTO application_type_has_overview( application_type_id, overview_id )
 SELECT application_type.id, overview.id
 FROM application_type, overview
 WHERE application_type.name IN ( "beartooth", "mastodon", "sabretooth" )
