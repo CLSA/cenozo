9154e5903a1adc7cd676140298ed2bb46a95d5b4
Patrick Emond <emondpd@mcmaster.ca>
2022-07-26 10:28:39 -0400

    Updating minified libs

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.8';
-$settings['general']['cenozo_build'] = '7a9581de';
+$settings['general']['cenozo_build'] = '07fa2f30';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
