393733f0666e55285a18b8e66c92708f44d8e043
Patrick Emond <emondpd@mcmaster.ca>
2022-12-09 12:58:37 -0500

    Adding new stripped and hover CSS classes

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.8';
-$settings['general']['cenozo_build'] = 'c7dcda2a';
+$settings['general']['cenozo_build'] = '83c7181d';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
