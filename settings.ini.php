6d40b5b8ccc2992451a4b616fd3d9f62a0a090be
Patrick Emond <emondpd@mcmaster.ca>
2023-07-19 14:25:46 -0400

    Tweak: removing debug statement

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.9';
-$settings['general']['cenozo_build'] = 'bbd319cb';
+$settings['general']['cenozo_build'] = '0b66bc0c';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
