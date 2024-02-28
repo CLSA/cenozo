7baf888178ced53b1c143f2332985a9a1f80d80c
Patrick Emond <emondpd@mcmaster.ca>
2024-02-28 09:25:23 -0500

    Tweak: cenozo.min.js was out of date

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.9';
-$settings['general']['cenozo_build'] = '679649f8';
+$settings['general']['cenozo_build'] = '2cb41c1b';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
