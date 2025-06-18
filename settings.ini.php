d293a7464da0b225e3d0f19def9714fc5e9f897b
Patrick Emond <emondpd@mcmaster.ca>
2025-06-18 11:56:45 -0400

    Bug: fixing typeahead regression caused by earlier form fix

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.10';
-$settings['general']['cenozo_build'] = 'dcc72ed6';
+$settings['general']['cenozo_build'] = '1456789d';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
