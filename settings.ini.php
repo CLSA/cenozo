b5c29d0bb1eee507efbd48818d482d1879fbc612
Patrick Emond <emondpd@mcmaster.ca>
2025-08-14 13:52:18 -0400

    Feature: adding active prop to application view/list

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.10';
-$settings['general']['cenozo_build'] = '1e277615';
+$settings['general']['cenozo_build'] = '48a443bc';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
