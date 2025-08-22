2e114eb546a886beb19c1d09d26942b96c1768c2
Patrick Emond <emondpd@mcmaster.ca>
2025-08-22 14:19:21 -0400

    v3: implementing participant scripts

--- web3/js/model/participant.mjs
+++ web3/js/model/participant.mjs
@@ -1,6 +1,9 @@
+import CN_api from "../api.mjs"
+import CN_common from "../common.mjs"
 import CN_element from "../element.mjs"
 import CN_session from "../session.mjs"
 
+import { CN_base_action } from "../base_action.mjs"
 import { CN_base_model } from "../base_model.mjs"
 import { CN_base_person_view, CN_base_person_history, CN_base_person_notes } from "../base_person_model.mjs"
 
@@ -234,3 +237,244 @@ export class CN_participant_view extends CN_base_person_view {
 export class CN_participant_history extends CN_base_person_history {}
 
 export class CN_participant_notes extends CN_base_person_notes {}
+
+export class CN_participant_scripts extends CN_base_action {
+  #script_list = [];
+  #reverse_messages = {
+    Proxy:
+      "Are you sure you wish to reverse the participant's proxy status?<br/><br/>" +
+      "By selecting yes you are confirming that the participant has decided to re-consider their proxy status.",
+    Withdraw:
+      "Are you sure you wish to reverse the participant's withdraw status?<br/><br/>" +
+      "By selecting yes you are confirming that the participant has re-consented to participate in the study.",
+  };
+
+  /**
+   * Constructor
+   * @param base_model model: The model that the action belongs to
+   */
+  constructor(model) {
+    super("notes", model);
+  }
+
+  /**
+   * Extend parent method
+   */
+  async get_text(type) {
+    if ("crumb" == type) {
+      const response = await CN_api.get(`participant/${this.get_model().get_identifier()}`, {
+        select: { column: "uid" },
+      });
+      return (await response.json()).uid;
+    }
+
+    if ("header" == type) {
+      const response = await CN_api.get(`participant/${this.get_model().get_identifier()}`, {
+        select: { column: ["uid", "first_name", "last_name"] },
+      });
+      const data = await response.json();
+      return `Utility scripts for ${data.first_name} ${data.last_name} (${data.uid})`;
+    }
+
+    return super.get_text(type);
+  }
+
+  /**
+   * Extend parent method
+   */
+  async on_navigate_to_parent() {
+    await CN_session.navigate_to(`participant/view/${this.get_model().get_identifier()}`);
+  }
+
+  /**
+   * Extend parent method
+   */
+  async on_load() {
+    await super.on_load();
+
+    // get the script list for this application
+    const response = await CN_api.get(`application/${CN_session.data.application.id}/script`, {
+      select: { column: ["id", "name", "url"] },
+      modifier: {
+        where: {
+          column: "supporting",
+          operator: "=",
+          value: true,
+        },
+        order: "name",
+      },
+    });
+    this.#script_list = await response.json();
+
+    // load the participant's status for all utility scripts in parallel
+    await Promise.all(
+      this.#script_list.map(script => {
+        const get_script_status = async () => {
+          script.token = null;
+          script.end_datetime = null;
+          try {
+            const response = await CN_api.get(
+              `script/${script.id}/pine_response/${this.get_model().get_identifier()}`
+            );
+            let data = await response.json();
+            script.token = data.token;
+            script.end_datetime = data.end_datetime;
+          } catch (error) {
+            // ignore 404
+            if (404 != error.response.status) throw error;
+          }
+        };
+
+        return get_script_status();
+      })
+    );
+  }
+
+  /**
+   * Extend parent method
+   */
+  update_element() {
+    const script_list_el = this.get_body_element().querySelector("[name=script_list]");
+
+    script_list_el.innerHTML = "";
+    this.#script_list.forEach(script => {
+      const reversable = this.#reverse_messages.hasOwnProperty(script.name);
+      const disabled = script.end_datetime && !reversable;
+      let title = `Launch ${script.name}`;
+
+      if (script.end_datetime) {
+        title = (
+          reversable ?
+          `Reverse ${script.name} (completed on ${CN_common.format_datetime(script.end_datetime, "datetime")})` :
+          `${script.name} Completed (${CN_common.format_datetime(script.end_datetime, "datetime")})`
+        );
+      }
+      const btn_el = CN_element.create(
+        `<button
+          type="button"
+          class="btn btn-outline-primary w-100"
+          ${disabled ? "disabled" : ""}
+        >${title}</button>`
+      );
+      btn_el.addEventListener("click", async () => {
+        if (script.end_datetime) {
+          if (reversable) {
+            const modal = CN_element.confirm_modal({
+              static: true,
+              title: `Reverse ${script.name}`,
+              message: this.#reverse_messages[script.name],
+            });
+
+            if (await modal.test()) {
+              await CN_element.wait_for(async () => {
+                const params = {};
+                params[`reverse_${script.name.replace(/ /, "_").toLowerCase()}`] = true;
+                await CN_api.patch(`participant/${this.get_model().get_identifier()}`, params);
+                await this.run();
+              });
+            }
+          }
+        } else {
+          // request a token if one doesn't already exist
+          if (null == script.token) {
+            await CN_element.wait_for(async () => {
+              const response = await CN_api.post(`script/${script.id}/pine_response`, {
+                identifier: this.get_model().get_identifier(),
+              })
+              script.token = (await response.json()).token;
+            });
+          }
+
+          // if we still don't have a token then there's a problem
+          if (null == script.token) {
+            CN_element.message_modal({
+              title: "Respondent Not Found",
+              message:
+                "Unable to find the respondent record belonging to the script you are trying to launch. " +
+                "If the problem persists please contact support.",
+              type: "danger",
+            }).show();
+          } else {
+            // launch the sript
+            const url_params = {
+              show_hidden: 1,
+              site: CN_session.data.site.name,
+              username: CN_session.data.user.name,
+            };
+            const params = (new URLSearchParams(url_params)).toString()
+            window.open(
+              `${script.url}${script.token}?${params}`,
+              `script_${script.id}`
+            ).focus();
+
+            // re-run the action once the user returns to this tab
+            const regained_focus = async () => {
+              await this.run();
+              window.removeEventListener("focus", regained_focus);
+            };
+            window.addEventListener("focus", regained_focus);
+          }
+        }
+      });
+      script_list_el.append(btn_el);
+    });
+  }
+
+  /**
+   * Extend parent method
+   */
+  create_placeholder_element() {
+    return CN_element.create(`
+      <div>
+        <div class="text-info pb-2">
+          Select which utility script you wish to launch on behalf of the participant.
+        </div>
+        <div name="script_list">
+          <button type="button" class="btn btn-outline-primary placeholder-glow w-100" disabled>
+            <span class="placeholder placeholder-lg col-${Math.ceil(Math.random()*3)+1}"></span>
+          </button>
+          <button type="button" class="btn btn-outline-primary placeholder-glow w-100" disabled>
+            <span class="placeholder placeholder-lg col-${Math.ceil(Math.random()*3)+1}"></span>
+          </button>
+          <button type="button" class="btn btn-outline-primary placeholder-glow w-100" disabled>
+            <span class="placeholder placeholder-lg col-${Math.ceil(Math.random()*3)+1}"></span>
+          </button>
+        </div>
+      </div>
+    `);
+  }
+
+  /**
+   * Extend parent method
+   */
+  create_body_element() {
+    const body_el = CN_element.create(`
+      <div>
+        <div class="text-info pb-2">
+          Select which utility script you wish to launch on behalf of the participant.
+        </div>
+        <div name="script_list"></div>
+      </div>
+    `);
+
+    return body_el;
+  }
+
+  /**
+   * Extend parent method
+   */
+  create_footer_element() {
+    const footer_el = CN_element.create(`
+      <div class="btn-group" role="group">
+        <button name="back" type="button" class="btn btn-primary">View Participant</button>
+      </div>
+    `);
+
+    footer_el.querySelector("button[name=back]").addEventListener(
+      "click",
+      async () => await this.on_navigate_to_parent()
+    );
+
+    return footer_el;
+  }
+}
