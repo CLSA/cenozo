b4e694d8e4f6a87f182b1baa9e82f8de6abbfb9a
Patrick Emond <emondpd@mcmaster.ca>
2025-09-24 11:18:59 -0400

    v3: implementing log_entry model

---
+++ web3/js/model/log_entry.mjs
@@ -0,0 +1,31 @@
+import { CN_base_model } from "../base_model.mjs"
+
+export class CN_log_entry_model extends CN_base_model {
+  constructor() {
+    super({
+      wording: {
+        singular: "log entry",
+        plural: "log entries",
+        posessive: "log entry's",
+      },
+      columns: {
+        datetime: { title: "Date & Time", type: "datetimesecond" },
+        type: { title: "Type" },
+        user: { title: "User", },
+        site: { title: "Site", },
+        role: { title: "Role", },
+        description: { title: "Description", type: "html", limit: 500 },
+      },
+      properties: {
+        datetime: { title: "Date & Time", type: "datetimesecond" },
+        type: { title: "Type" },
+        user: { title: "User" },
+        site: { title: "Site" },
+        role: { title: "Role" },
+        service: { title: "Service" },
+        description: { title: "Description", type: "text" },
+        stack_trace: { title: "Stack Trace", type: "text" },
+      },
+    });
+  }
+}
