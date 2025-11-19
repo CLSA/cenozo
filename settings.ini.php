ce540d2f7f6dce2a2b56887107a00b3b907014a1
Patrick Emond <emondpd@mcmaster.ca>
2025-11-19 15:31:42 -0500

    Feature: replacing prev/next buttons with first/last in pagination controls

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.10';
-$settings['general']['cenozo_build'] = '7c893ba6';
+$settings['general']['cenozo_build'] = '56b69a3f';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
