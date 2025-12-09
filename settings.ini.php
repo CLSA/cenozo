3455709cda6c0d953026f01814b71b79a662d5fe
Patrick Emond <emondpd@mcmaster.ca>
2025-12-09 13:42:47 -0500

    Bug: file-type inputs weren't respecting mandatory property

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '2.10';
-$settings['general']['cenozo_build'] = '56b69a3f';
+$settings['general']['cenozo_build'] = 'abea3018';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
