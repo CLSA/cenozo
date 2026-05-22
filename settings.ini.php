c735a1581e50b5cbc5208f5c2c6728632aca8a48
Patrick Emond <emondpd@mcmaster.ca>
2026-05-22 13:51:30 -0400

    Bug: force-reset of password not working

--- settings.ini.php
+++ settings.ini.php
@@ -14,7 +14,7 @@ $settings = array();
 
 // framework software version (is never overridded by the application's ini file)
 $settings['general']['cenozo_version'] = '3.0';
-$settings['general']['cenozo_build'] = '57847d9b';
+$settings['general']['cenozo_build'] = '54975b87';
 
 // when set to true all operations are disabled
 $settings['general']['maintenance_mode'] = false;
