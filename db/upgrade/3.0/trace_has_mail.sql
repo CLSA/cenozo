8ec00aa077c47302ce8cef03da8894e41edd080d
Patrick Emond <emondpd@mcmaster.ca>
2026-07-09 16:24:08 -0400

    Bug: removing errors when patching magnolia's cenozo database

--- db/upgrade/3.0/trace_has_mail.sql
+++ db/upgrade/3.0/trace_has_mail.sql
@@ -1,24 +1,42 @@
-SELECT "Creating new trace_has_mail table" AS "";
+DROP PROCEDURE IF EXISTS patch_trace_has_mail;
+DELIMITER //
+CREATE PROCEDURE patch_trace_has_mail()
+  BEGIN
 
-CREATE TABLE IF NOT EXISTS trace_has_mail (
-  trace_id INT(10) UNSIGNED NOT NULL,
-  mail_id INT(10) UNSIGNED NOT NULL,
-  update_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-  create_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (trace_id, mail_id),
-  UNIQUE INDEX uq_trace_id_mail_id (trace_id ASC, mail_id ASC),
-  INDEX fk_mail_id (mail_id ASC),
-  INDEX fk_trace_id (trace_id ASC),
-  CONSTRAINT fk_trace_has_mail_trace_id
-    FOREIGN KEY (trace_id)
-    REFERENCES trace (id)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_trace_has_mail_mail_id
-    FOREIGN KEY (mail_id)
-    REFERENCES mail (id)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_general_ci;
+    SELECT COUNT(*) INTO @test
+    FROM information_schema.TABLES
+    WHERE table_schema = DATABASE()
+    AND table_name IN ("mail", "trace");
+
+    IF @test = 2 THEN
+      SELECT "Creating new trace_has_mail table" AS "";
+
+      CREATE TABLE IF NOT EXISTS trace_has_mail (
+        trace_id INT(10) UNSIGNED NOT NULL,
+        mail_id INT(10) UNSIGNED NOT NULL,
+        update_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+        create_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+        PRIMARY KEY (trace_id, mail_id),
+        UNIQUE INDEX uq_trace_id_mail_id (trace_id ASC, mail_id ASC),
+        INDEX fk_mail_id (mail_id ASC),
+        INDEX fk_trace_id (trace_id ASC),
+        CONSTRAINT fk_trace_has_mail_trace_id
+          FOREIGN KEY (trace_id)
+          REFERENCES trace (id)
+          ON DELETE CASCADE
+          ON UPDATE NO ACTION,
+        CONSTRAINT fk_trace_has_mail_mail_id
+          FOREIGN KEY (mail_id)
+          REFERENCES mail (id)
+          ON DELETE CASCADE
+          ON UPDATE NO ACTION)
+      ENGINE = InnoDB
+      DEFAULT CHARACTER SET = utf8mb4
+      COLLATE = utf8mb4_general_ci;
+    END IF;
+
+  END //
+DELIMITER ;
+
+CALL patch_trace_has_mail();
+DROP PROCEDURE IF EXISTS patch_trace_has_mail;
--- db/upgrade/3.0/trace_type_mail.sql
+++ db/upgrade/3.0/trace_type_mail.sql
@@ -1,32 +1,50 @@
-SELECT "Creating new trace_type_mail table" AS "";
+DROP PROCEDURE IF EXISTS patch_trace_type_mail;
+DELIMITER //
+CREATE PROCEDURE patch_trace_type_mail()
+  BEGIN
 
-CREATE TABLE IF NOT EXISTS trace_type_mail (
-  id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  update_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-  create_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  trace_type_id INT(10) UNSIGNED NULL DEFAULT NULL,
-  language_id INT(10) UNSIGNED NOT NULL,
-  from_name VARCHAR(255) NULL DEFAULT NULL,
-  from_address VARCHAR(127) NOT NULL,
-  cc_address VARCHAR(255) NULL DEFAULT NULL,
-  bcc_address VARCHAR(255) NULL DEFAULT NULL,
-  delay_offset INT(10) UNSIGNED NOT NULL DEFAULT 1,
-  delay_unit ENUM('days', 'weeks', 'months') NOT NULL DEFAULT 'weeks',
-  subject VARCHAR(255) NOT NULL,
-  body MEDIUMTEXT NOT NULL,
-  PRIMARY KEY (id),
-  INDEX fk_trace_type_id (trace_type_id ASC),
-  INDEX fk_language_id (language_id ASC),
-  CONSTRAINT fk_trace_type_mail_trace_type_id
-    FOREIGN KEY (trace_type_id)
-    REFERENCES trace_type (id)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_trace_type_mail_language_id
-    FOREIGN KEY (language_id)
-    REFERENCES language (id)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_general_ci;
+    SELECT COUNT(*) INTO @test
+    FROM information_schema.TABLES
+    WHERE table_schema = DATABASE()
+    AND table_name = "trace_type";
+
+    IF @test = 1 THEN
+      SELECT "Creating new trace_type_mail table" AS "";
+
+      CREATE TABLE IF NOT EXISTS trace_type_mail (
+        id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+        update_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+        create_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+        trace_type_id INT(10) UNSIGNED NULL DEFAULT NULL,
+        language_id INT(10) UNSIGNED NOT NULL,
+        from_name VARCHAR(255) NULL DEFAULT NULL,
+        from_address VARCHAR(127) NOT NULL,
+        cc_address VARCHAR(255) NULL DEFAULT NULL,
+        bcc_address VARCHAR(255) NULL DEFAULT NULL,
+        delay_offset INT(10) UNSIGNED NOT NULL DEFAULT 1,
+        delay_unit ENUM('days', 'weeks', 'months') NOT NULL DEFAULT 'weeks',
+        subject VARCHAR(255) NOT NULL,
+        body MEDIUMTEXT NOT NULL,
+        PRIMARY KEY (id),
+        INDEX fk_trace_type_id (trace_type_id ASC),
+        INDEX fk_language_id (language_id ASC),
+        CONSTRAINT fk_trace_type_mail_trace_type_id
+          FOREIGN KEY (trace_type_id)
+          REFERENCES trace_type (id)
+          ON DELETE CASCADE
+          ON UPDATE NO ACTION,
+        CONSTRAINT fk_trace_type_mail_language_id
+          FOREIGN KEY (language_id)
+          REFERENCES language (id)
+          ON DELETE CASCADE
+          ON UPDATE NO ACTION)
+      ENGINE = InnoDB
+      DEFAULT CHARACTER SET = utf8mb4
+      COLLATE = utf8mb4_general_ci;
+    END IF;
+
+  END //
+DELIMITER ;
+
+CALL patch_trace_type_mail();
+DROP PROCEDURE IF EXISTS patch_trace_type_mail;
